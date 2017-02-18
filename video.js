5ebb34c90e2542b53ac839b2eef62254f5a0879b
unknown <rxm97520@ucmo.edu>
2017-02-17 23:32:49 -0600

    Added log

--- video.js
+++ video.js
@@ -8,5 +8,6 @@ http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type': 'video/mp4'});
 	var rs = fs.createReadStream("tom.mp4");
 	rs.pipe(response);
-	console.log('Express server listening on port 3005');
 }).listen(3005);
+
+console.log('Server listening on port 3005');
